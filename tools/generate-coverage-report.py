4e874c589013806967b41d30fabb389c8fe6b283
Furkan Kurt <kurtfu@protonmail.com>
2023-12-12 00:05:31 +0300

    build: change coverage report output directory

--- tools/generate-coverage-report.py
+++ tools/generate-coverage-report.py
@@ -21,6 +21,7 @@ root = os.path.dirname(__file__)
 root = os.path.dirname(root)
 
 output = os.path.join(root, 'docs')
+output = os.path.join(output, 'coverage')
 output = os.path.join(output, 'coverage.html')
 
 if not os.path.exists(os.path.dirname(output)):
@@ -30,5 +31,5 @@ gcov = 'llvm-cov gcov' if args.compiler == 'clang' else 'gcov'
 
 subprocess.run(['gcovr', '--gcov-executable', gcov, '--html-details',
                 '-r', root, '-o', output,
-                '--exclude', '.*_deps.*', '--exclude', '.*tests.*'
+                '--exclude', '.*build.*', '--exclude', '.*tests.*'
                 ])
