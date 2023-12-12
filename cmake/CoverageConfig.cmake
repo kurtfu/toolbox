12cd1067af94a33e421e32e5c78357bbd0f87380
Furkan Kurt <kurtfu@protonmail.com>
2023-12-12 23:40:01 +0300

    build: add coverage generation target

--- cmake/CoverageConfig.cmake
+++ cmake/CoverageConfig.cmake
@@ -1,5 +1,6 @@
 find_program(GCOV_BIN gcov)
 find_program(LCOV_BIN lcov)
+find_program(GCOVR_BIN gcovr)
 
 if(NOT GCOV_BIN)
     message(FATAL_ERROR "gcov not found! Aborting...")
@@ -8,6 +9,21 @@ endif()
 if(NOT LCOV_BIN)
     message(FATAL_ERROR "lcov not found! Aborting...")
 endif()
+
+if(NOT GCOVR_BIN)
+    message(FATAL_ERROR "gcovr not found! Aborting...")
+endif()
+
+add_custom_target(coverage
+    COMMAND
+        ${CMAKE_COMMAND} -E make_directory docs/coverage
+    COMMAND
+        ${GCOVR_BIN} --html-details -o docs/coverage/index.html -e .*build.* -e .*tests.*
+    WORKING_DIRECTORY
+        ${PROJECT_SOURCE_DIR}
+    COMMENT
+        "Generating code coverage report"
+)
 
 function(setup_target_for_coverage target)
     cleanup_coverage_data(${target})
@@ -19,7 +35,9 @@ function(cleanup_coverage_data target)
         TARGET
             ${target}
         COMMAND
-            ${LCOV_BIN} --gcov-tool ${GCOV_BIN} -directory . -b ${PROJECT_SOURCE_DIR} --zerocounters
+            ${LCOV_BIN} --zerocounters --directory .
+        WORKING_DIRECTORY
+            ${PROJECT_SOURCE_DIR}
     )
 endfunction()
 
