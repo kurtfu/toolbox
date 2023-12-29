bdef729d39cd890b84a05e969304bb8814d304c3
Furkan Kurt <kurtfu@protonmail.com>
2023-12-30 00:14:05 +0300

    test: add value and error type tests

--- tests/result.cpp
+++ tests/result.cpp
@@ -14,24 +14,40 @@
 
 result<int, std::string> good()
 {
-    return success<int>{};
+    return success<int>(0);
 }
 
 result<int, std::string> bad()
 {
-    return fail<std::string>{};
+    return fail<std::string>("bad");
 }
 
-TEST_CASE("Success")
+TEST_CASE("Simple success assignment")
 {
     auto ret = good();
 
     REQUIRE(ret.has_value() == true);
+    REQUIRE_NOTHROW(ret.value() == 0);
 }
 
-TEST_CASE("Fail")
+TEST_CASE("Simple fail assignment")
 {
     auto ret = bad();
 
     REQUIRE(ret.has_value() == false);
+    REQUIRE_NOTHROW(ret.error() == "bad");
+}
+
+TEST_CASE("Bad value access")
+{
+    auto ret = bad();
+
+    REQUIRE_THROWS_AS(ret.value(), bad_value_exception);
+}
+
+TEST_CASE("Bad error access")
+{
+    auto ret = good();
+
+    REQUIRE_THROWS_AS(ret.error(), bad_value_exception);
 }
