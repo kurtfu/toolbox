6b67bd3c46d7dfc81788056865a6036a5d8ee3cc
Furkan Kurt <kurtfu@protonmail.com>
2024-02-16 22:11:06 +0300

    build: add optimization configuration

---
+++ cmake/Optimization.cmake
@@ -0,0 +1,14 @@
+option(ENABLE_IPO "Enable Interprocedural Optimizations for project targets" OFF)
+
+if (NOT ENABLE_IPO)
+    return()
+endif()
+
+include(CheckIPOSupported)
+check_ipo_supported(RESULT result OUTPUT output)
+
+if(result)
+    set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
+else()
+    message(STATUS "IPO is not supported: ${output}")
+endif()
