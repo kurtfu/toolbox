9bf100dabccbc5d9f4ccb76d8cf0510b2722a2f3
Furkan Kurt <kurtfu@protonmail.com>
2024-02-19 22:37:38 +0300

    build: add utility configuration

---
+++ cmake/Utils.cmake
@@ -0,0 +1,51 @@
+include(Coverage)
+include(Sanitizer)
+include(Testing)
+
+function(setup_executable)
+    set(oneValueArgs TARGET)
+    set(multiValueArgs SOURCES INCLUDES LIBRARIES)
+
+    cmake_parse_arguments(EXECUTABLE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    _setup_executable_sources(${EXECUTABLE_TARGET} ${EXECUTABLE_SOURCES})
+    _setup_target_includes(${EXECUTABLE_TARGET} ${EXECUTABLE_INCLUDES})
+    _setup_target_libraries(${EXECUTABLE_TARGET} ${EXECUTABLE_LIBRARIES})
+
+    setup_target_for_sanitizer(${EXECUTABLE_TARGET})
+endfunction()
+
+function(setup_test)
+    set(oneValueArgs TARGET)
+    set(multiValueArgs SOURCES INCLUDES LIBRARIES)
+
+    cmake_parse_arguments(EXECUTABLE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    _setup_executable_sources(${EXECUTABLE_TARGET} ${EXECUTABLE_SOURCES})
+    _setup_target_includes(${EXECUTABLE_TARGET} ${EXECUTABLE_INCLUDES})
+    _setup_target_libraries(${EXECUTABLE_TARGET} ${EXECUTABLE_LIBRARIES})
+
+    setup_target_for_coverage(${EXECUTABLE_TARGET})
+    setup_target_for_sanitizer(${EXECUTABLE_TARGET})
+    setup_target_for_testing(${EXECUTABLE_TARGET})
+endfunction()
+
+function(_setup_executable_sources target)
+    add_executable(${target}
+        ${ARGN}
+    )
+endfunction()
+
+function(_setup_target_includes target)
+    target_include_directories(${target}
+        PRIVATE
+            ${ARGN}
+    )
+endfunction()
+
+function(_setup_target_libraries target)
+    target_link_libraries(${target}
+        PRIVATE
+            ${ARGN}
+    )
+endfunction()
