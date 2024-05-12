ec71ef0abffb2faecdaecf6f3afc9f91bd46bb71
Furkan Kurt <kurtfu@protonmail.com>
2024-05-12 16:33:31 +0300

    build: add test framework check

--- cmake/Testing.cmake
+++ cmake/Testing.cmake
@@ -1,9 +1,15 @@
 find_package(Catch2)
 
-include(Catch)
-enable_testing()
+if(Catch2_FOUND)
+    include(Catch)
+    enable_testing()
+endif()
 
 function(setup_target_for_testing target)
+    if(NOT Catch2_FOUND)
+        return()
+    endif()
+
     target_link_libraries(${target}
         PRIVATE
             Catch2::Catch2WithMain
