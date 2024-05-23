6db59ba3268678a6935cf68637a7fb1043c0f5f5
Furkan Kurt <kurtfu@protonmail.com>
2024-05-23 22:41:23 +0300

    build: add miscellaneous compilation options

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -2,6 +2,24 @@ include(Linker)
 include(Sanitizer)
 include(Warnings)
 
+set(CLANG_CXX_MISC_OPTIONS
+    -save-temps
+)
+
+set(GCC_CXX_MISC_OPTIONS
+    ${CLANG_CXX_MISC_OPTIONS}
+)
+
+if("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
+    set(PROJECT_CXX_MISC_OPTIONS ${CLANG_CXX_MISC_OPTIONS})
+elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+    set(PROJECT_CXX_MISC_OPTIONS ${GCC_CXX_MISC_OPTIONS})
+else()
+    if("CXX" IN_LIST languages)
+        message(STATUS "No extra options set for CXX compiler: '${CMAKE_CXX_COMPILER_ID}'")
+    endif()
+endif()
+
 function(setup_executable)
     set(oneValueArgs TARGET)
     set(multiValueArgs SOURCES INCLUDES LIBRARIES)
@@ -11,6 +29,7 @@ function(setup_executable)
     _setup_executable_sources(${EXECUTABLE_TARGET} ${EXECUTABLE_SOURCES})
     _setup_target_includes(${EXECUTABLE_TARGET} ${EXECUTABLE_INCLUDES})
     _setup_target_libraries(${EXECUTABLE_TARGET} ${EXECUTABLE_LIBRARIES})
+    _setup_misc_options(${EXECUTABLE_TARGET})
 
     setup_target_link_strategy(${EXECUTABLE_TARGET})
 
@@ -35,5 +54,12 @@ macro(_setup_target_libraries target)
     target_link_libraries(${target}
         PRIVATE
             ${ARGN}
+    )
+endmacro()
+
+macro(_setup_misc_options target)
+    target_compile_options(${target}
+        PRIVATE
+            ${PROJECT_CXX_MISC_OPTIONS}
     )
 endmacro()
