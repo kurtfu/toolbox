dddfc5a3cc48b8bfe592f225c6eddea8d6fa91aa
Furkan Kurt <kurtfu@protonmail.com>
2025-01-17 15:40:08 +0300

    build: add target dependecy options

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -3,13 +3,14 @@ include(Sanitizer)
 include(Warnings)
 
 function(setup_executable target)
-    set(multiValueArgs SOURCES INCLUDES DEPENDENCIES INSTALL PROPERTIES)
+    set(multiValueArgs SOURCES INCLUDES DEPENDENCIES INSTALL PROPERTIES DEFINES)
 
     cmake_parse_arguments(TARGET "" "" "${multiValueArgs}" ${ARGN})
 
     _setup_executable_sources(${target})
     _setup_target_includes(${target} PUBLIC)
     _setup_target_dependencies(${target})
+    _setup_target_defines(${target})
 
     if (DEFINED TARGET_PROPERTIES)
         _setup_target_properties(${target})
@@ -26,13 +27,18 @@ function(setup_executable target)
 endfunction()
 
 function(setup_library target)
-    set(multiValueArgs TYPE SOURCES INCLUDES DEPENDENCIES INSTALL PROPERTIES)
+    set(multiValueArgs TYPE SOURCES INCLUDES DEPENDENCIES INSTALL PROPERTIES DEFINES)
 
     cmake_parse_arguments(TARGET "" "" "${multiValueArgs}" ${ARGN})
 
-    _setup_library_sources(${target})
+    if(NOT DEFINED TARGET_TYPE)
+        set(TARGET_TYPE STATIC)
+    endif()
+
+    _setup_library_sources(${target} ${TARGET_TYPE})
     _setup_target_includes(${target} PRIVATE)
     _setup_target_dependencies(${target})
+    _setup_target_defines(${target})
 
     if (DEFINED TARGET_PROPERTIES)
         _setup_target_properties(${target})
@@ -91,15 +97,45 @@ macro(_setup_target_includes target scope)
 endmacro()
 
 macro(_setup_target_dependencies target)
+    set(DEPENDENCY_DIRECTORIES "")
+    set(DEPENDENCY_LIBRARIES "")
+
+    set(IS_DIRECTORY FALSE)
+
+    foreach(token IN LISTS TARGET_DEPENDENCIES)
+        if(token STREQUAL "DIRECTORY")
+            set(IS_DIRECTORY TRUE)
+        else()
+            if(NOT IS_DIRECTORY)
+                list(APPEND DEPENDENCY_LIBRARIES ${token})
+            else()
+                list(APPEND DEPENDENCY_DIRECTORIES ${token})
+                set(IS_DIRECTORY FALSE)
+            endif()
+        endif()
+    endforeach()
+
+    target_link_directories(${target}
+        PRIVATE
+            ${DEPENDENCY_DIRECTORIES}
+    )
+
     target_link_libraries(${target}
         PRIVATE
-            ${TARGET_DEPENDENCIES}
+            ${DEPENDENCY_LIBRARIES}
     )
 endmacro()
 
-function(_setup_target_properties target)
+macro(_setup_target_defines target)
+    target_compile_definitions(${target}
+        PRIVATE
+            ${TARGET_DEFINES}
+    )
+endmacro()
+
+macro(_setup_target_properties target)
     set_target_properties(${target}
         PROPERTIES
             ${TARGET_PROPERTIES}
     )
-endfunction()
+endmacro()
