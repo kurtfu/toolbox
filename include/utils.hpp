baabcf8ba4e2174bb6d0b6e56838df8adb0eefeb
Furkan Kurt <kurtfu@protonmail.com>
2025-01-25 17:22:49 +0300

    feat: add timed repeat function

---
+++ include/utils.hpp
@@ -0,0 +1,35 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+/*****************************************************************************/
+/*** HEADER INCLUDES *********************************************************/
+
+/// \cond
+#include <chrono>
+#include <functional>
+#include <utility>
+
+/// \endcond
+
+/*****************************************************************************/
+/*** MACRO DEFINITIONS *******************************************************/
+
+#define panic(message) annotate_and_terminate(message, __FILE__, __LINE__)
+
+/*****************************************************************************/
+/*** FUNCTION DEFINITIONS ****************************************************/
+
+template <typename Duration, typename Period, typename F, typename... Args>
+void repeat_for(Period period, F&& fn, Args&&... args)
+{
+    auto start = std::chrono::steady_clock::now();
+
+    while (std::chrono::steady_clock::now() - start < Duration(period))
+    {
+        std::invoke(std::forward<F>(fn), std::forward<Args>(args)...);
+    }
+}
+
+void annotate_and_terminate(const char* message, const char* file, int line);
+
+#endif  // UTILS_HPP
