c1addfdd17c117b7689930a43c12b5a8c00191ac
Furkan Kurt <kurtfu@protonmail.com>
2025-12-12 23:41:25 +0300

    build: move cli module dependencies

--- projects/toolbox.cmake
+++ projects/toolbox.cmake
@@ -1,12 +1,19 @@
 find_package(quill)
+find_package(replxx)
+find_package(Threads)
+
+find_path(REPLXX_INCLUDE_DIR NAMES "replxx.hxx")
 
 setup_library(toolbox
     SOURCES
         src/utils.cpp
     INCLUDES
         include
+        SYSTEM ${REPLXX_INCLUDE_DIR}
     DEPENDENCIES
         quill::quill
+        replxx::replxx
+        Threads::Threads
 )
 
 setup_executable(toolbox-test
